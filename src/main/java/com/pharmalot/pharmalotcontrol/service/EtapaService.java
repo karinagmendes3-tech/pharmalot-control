dd218659a2bf2b448aed6d85e11ee7df5a75d451
Karina de Aguiar <karina.gmendes3@gmail.com>
2026-05-13 20:04:21 -0300

    Corrige Home Controller FINAL

--- src/main/java/com/pharmalot/pharmalotcontrol/service/EtapaService.java
+++ src/main/java/com/pharmalot/pharmalotcontrol/service/EtapaService.java
@@ -16,6 +16,6 @@ public class EtapaService {
     }
 
     public List<EtapaProducao> buscarPorOp(String op) {
-        return etapaRepository.findByOp(op);
+        return etapaRepository.findByOpContainingIgnoreCase(op);
     }
 }
