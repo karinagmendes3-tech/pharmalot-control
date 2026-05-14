a871b056794432116540f7943055e4f3804bbea1
Karina de Aguiar <karina.gmendes3@gmail.com>
2026-05-13 21:43:54 -0300

    ajuste layout login

--- src/main/java/com/pharmalot/pharmalotcontrol/SecurityConfig.java
+++ src/main/java/com/pharmalot/pharmalotcontrol/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/css/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
